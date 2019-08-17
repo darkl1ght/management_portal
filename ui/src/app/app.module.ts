13f645cfac8c1e46bd6a565711e2b6c08a21b09a
Ayush Chauhan <ayoushchauhan1996@gmail.com>
2019-08-17 17:15:18 +0530

    modify ui

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,28 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { HomeModule } from './home/home.module';
-import {
-  FooterComponent,
-  HeaderComponent,
-  SharedModule
-} from './shared';
 import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    CoreModule,
-    SharedModule,
-    HomeModule,
-    AuthModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
